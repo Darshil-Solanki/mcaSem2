8c66df4db4afe2912068eea5ee32046f75481e4e
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-02-08 10:01:50 +0530

    Add files via upload

---
+++ Practical/PythonClasswork/demo.py
@@ -0,0 +1,15 @@
+x=float(1)
+y=int(2.8)
+z=complex(x)
+#print(x)
+#print(y)
+#print(z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+st="Python Programming"
+len=0
+for i in st:
+	len+=1
+print("Length of st is: ",len)
+print("gram is", 'present' if 'gram' in st else 'not present')
